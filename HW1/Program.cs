dbe18b952260b7aae9aa7e444890c4ec9f4af317
Your Name <excaliburdotcom@gmail.com>
2025-09-09 13:26:10 +0800

    HW1

---
+++ HW1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace HW1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text;
+
+            do
+            {
+                Console.Write("Enter String: ");
+                text = Console.ReadLine();
+
+                string lower = text.ToLower();
+                string reversed = "";
+
+                for (int i = lower.Length - 1; i >= 0; i--)
+                {
+                    reversed += lower[i];
+                }
+                string output = (lower == reversed) ? "Yes" : "No";
+                Console.WriteLine("Original          Reversed          Palindrome");
+                Console.WriteLine("{0,-20}{1,-20} {2}", text, reversed, output);
+                
+            } while (text != "*");
+            Console.ReadKey();
+        }
+    }
+}
